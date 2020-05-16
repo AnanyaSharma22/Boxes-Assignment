ce2677ca3bafa5af62087b13ad2cc657a2a1972a
Ananya Sharma <Ananya Sharma>
2020-05-16 22:35:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  public images =[];
+  path1:any; 
+  path2:any;
+  path3:any;
+  path4:any;
+  path5:any;
+  path6:any;
+  constructor(
+  ){}
+  
+  ngOnInit(): void {
+    this.initializeImageArray();
+  }
+
+  initializeImageArray(){
+    this.images = [{path: '/assets/images/car.jpg', description: 'image 1'}, {path: '/assets/images/cars_1.jpg', description: 'image 2'},
+    {path: '/assets/images/cars_2.jpg', description: 'image 3'}, {path: '/assets/images/cars_3.jpg', description: 'image 4'},
+    {path: '/assets/images/cars_4.jpg', description: 'image 5'}, {path: '/assets/images/cars_5.jpeg', description: 'image 6'}
+    ];
+  }
+
+  public remove(index: number) {
+    this.images.splice(index, 1);
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
+  exports: [
+  ],
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
